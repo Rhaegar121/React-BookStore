8491e83985dd67af402a115d55a677b87f560fcd
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-21 22:30:03 +0630

    add form component

--- src/compoents/Book.js
+++ src/compoents/Book.js
@@ -1,10 +1,15 @@
+import Form from "./Form";
+
 const Book = ({title, author}) => {
     return (
+        <>
         <div>
             <span>{title}</span>
             <span>{author}</span>
             <button>Remove</button>
         </div>
+        <Form />
+        </>
     )
 }
 
---
+++ src/compoents/Form.js
@@ -0,0 +1,12 @@
+const Form = () => {
+    return (
+        <Form>
+            <h1>Add new book</h1>
+            <input type="text" placeholder="Title" />
+            <input type="text" placeholder="Author" />
+            <button type="button">Add</button>
+        </Form>
+    )
+}
+
+export default Form;
