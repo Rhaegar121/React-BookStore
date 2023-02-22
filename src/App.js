179c37dbd0af1e44f7aced70ebfc903611682eed
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-22 14:16:50 +0630

    change App function to ES6

--- src/App.js
+++ src/App.js
@@ -6,18 +6,16 @@ import Layout from './compoents/Layout';
 import Book from './compoents/Book';
 import Categories from './compoents/Categories';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Book title="book1" author="Barry" />} />
-          <Route path="categories" element={<Categories />} />
-          <Route path="*" element={<h1>Not found</h1>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Book title="book1" author="Barry" />} />
+        <Route path="categories" element={<Categories />} />
+        <Route path="*" element={<h1>Not found</h1>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
--- src/compoents/Layout.js
+++ src/compoents/Layout.js
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 
-const Layout = () => {
-    return (
-        <>
-          <Navbar />
-          <Outlet />
-        </>
-      );
-}
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
 
 export default Layout;
