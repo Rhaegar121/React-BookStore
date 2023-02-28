466ae35342675534f6684f022ceff637406186e7
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-28 20:16:29 +0630

    add checkStatus action in categories

--- src/compoents/Categories.js
+++ src/compoents/Categories.js
@@ -1,5 +1,15 @@
-const Categories = () => (
-  <button type="button">Check Status</button>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categoriesSlice';
+
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <button type="button" onClick={() => dispatch(checkStatus())}>Check Status</button>
+      <span>{status}</span>
+    </>
+  );
+};
 
 export default Categories;
