323af95d7aa6e550008ebe536be574b54bdc358c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-03-02 21:39:32 +0630

    install react icons

--- src/compoents/Navbar.js
+++ src/compoents/Navbar.js
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar">
-    <ul className="navLink">
-      <li><Link to="/">Books</Link></li>
-      <li><Link to="/categories">Categories</Link></li>
-    </ul>
+    <div className="navLink">
+      <h1>Bookstore CMS</h1>
+      <span><Link to="/">Books</Link></span>
+      <span><Link to="/categories">Categories</Link></span>
+    </div>
+    <div className="user">user</div>
   </nav>
 );
 
