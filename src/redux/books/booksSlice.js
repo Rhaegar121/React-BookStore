249c3f59a1dbe0f140a8c3b8c6a56a6ba329fcd1
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-03-05 23:09:20 +0630

    update booksSlice reducer

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,19 +3,33 @@ import { v4 as uuidv4 } from 'uuid';
 
 const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Z5WyGB77q5Knh8vVdrbr/books';
 
+// const initialState = {
+//   book: [],
+// };
+
 export const fetchBook = createAsyncThunk('fetchBook', async () => {
   const res = await fetch(url);
   const data = await res.json();
-  const bookList = [];
-  Object.keys(data).forEach((key) => {
-    bookList.push({
+  const bookList = Object.keys(data).map((key) => (
+    {
       item_id: key,
       title: data[key][0].title,
       author: data[key][0].author,
       category: data[key][0].category,
-    });
-  });
+    }
+  ));
+  // console.log(bookList);
   return bookList;
+  // const bookList = [];
+  // Object.keys(data).forEach((key) => {
+  //   bookList.push({
+  //     item_id: key,
+  //     title: data[key][0].title,
+  //     author: data[key][0].author,
+  //     category: data[key][0].category,
+  //   });
+  // });
+  // return bookList;
 });
 
 export const addBook = createAsyncThunk('addBook', async (book) => {
@@ -49,17 +63,23 @@ const booksSlice = createSlice({
   initialState: [],
   extraReducers: (builder) => {
     builder
-      .addCase(fetchBook.fulfilled, (state, action) => ({
-        ...state,
-        book: [...action.payload],
-      }))
+      // .addCase(fetchBook.fulfilled, (state, action) => ({
+      //   ...state,
+      //   book: [...action.payload],
+      // }))
+      // .addCase(addBook.fulfilled, (state, action) => {
+      //   state.book.push(action.payload);
+      // })
+      // .addCase(removeBook.fulfilled, (state, action) => ({
+      //   ...state,
+      //   book: state.book.filter((book) => book.item_id !== action.payload),
+      // }));
+      .addCase(fetchBook.fulfilled, (state, action) => action.payload)
       .addCase(addBook.fulfilled, (state, action) => {
-        state.book.push(action.payload);
+        state.push(action.payload);
       })
-      .addCase(removeBook.fulfilled, (state, action) => ({
-        ...state,
-        book: state.book.filter((book) => book.item_id !== action.payload),
-      }));
+      .addCase(removeBook.fulfilled, (state, action) => (
+        [...state.filter((book) => book.item_id !== action.payload)]));
   },
 });
 
